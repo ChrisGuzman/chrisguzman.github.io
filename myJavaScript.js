5543e79226200f21cbc7cc3e6d6e3ccbdcc140a9
Chris Guzman <cguzman@orderup.com>
2016-01-31 15:02:02 -0500

    Add css and pictures
    TODO: add some javascript later

---
+++ myJavaScript.js
@@ -0,0 +1,13 @@
+var temperature = 25;
+if (temperature < 50) {
+  console.log("Wear a coat.");
+}
+else if (temperature < 30) {
+  console.log("Wear a coat and a hat.");
+}
+else if (temperature < 0) {
+  console.log("Stay inside");
+}
+else{
+  console.log("Wear whatever you want.");
+};
